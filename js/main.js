99e9cef85bc74b46b33fdc7f5918a64b032e3512
Victor Paranhos <victor_paranhos@hotmail.com>
2023-02-02 18:16:46 -0300

    02 Noite

--- js/main.js
+++ js/main.js
@@ -2,25 +2,36 @@ import { Caixinhas } from './classes.js'
 new Caixinhas()
 
 
+
+
 const arraysDiv = document.querySelectorAll('.sec-5-caixa-duvidas .containers-duvidas')
+const PDuvida = document.querySelectorAll('.paragrafo-duvida')
 const icone = document.querySelectorAll('.icone')
 
-for(let array of arraysDiv)  {
-  let confere = true
-  
+
+arraysDiv.forEach(array => {
+
   array.addEventListener('click', () => {
-    if(confere){
+    const isOK = array.querySelector('.paragrafo-duvida').classList.contains('active')
+
+    icone.forEach(icones => icones.innerHTML = '&#43;')
+
+    PDuvida.forEach(deletee => {
+      deletee.classList.remove('active')
+
+    } )
+
+    if (isOK) {
+      array.querySelector('.paragrafo-duvida').classList.remove('active')
+    } else {
+ 
       array.querySelector('.paragrafo-duvida').classList.add('active')
       array.querySelector('.icone').innerHTML = '&minus;'
-      confere = false
-    }else {
-      array.querySelector('.paragrafo-duvida').classList.remove('active')
-      array.querySelector('.icone').innerHTML = '&#43;'
-      confere = true
     }
+
     
-   
   })
 
-   
-}
+
+})
+
