82c99d21519bfc35aae41353c1f9b66d13190b1b
Victor Paranhos <victor_paranhos@hotmail.com>
2023-02-02 17:05:37 -0300

    Up

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-import { Caixinhas } from "./classes.js";
+import { Caixinhas } from './classes.js'
 new Caixinhas()
 
 
+const arraysDiv = document.querySelectorAll('.sec-5-caixa-duvidas .containers-duvidas')
+const icone = document.querySelectorAll('.icone')
 
+for(let array of arraysDiv)  {
+  let confere = true
+  
+  array.addEventListener('click', () => {
+    if(confere){
+      array.querySelector('.paragrafo-duvida').classList.add('active')
+      array.querySelector('.icone').innerHTML = '&minus;'
+      confere = false
+    }else {
+      array.querySelector('.paragrafo-duvida').classList.remove('active')
+      array.querySelector('.icone').innerHTML = '&#43;'
+      confere = true
+    }
+    
+   
+  })
 
-
-const btnDuvidas = document.querySelectorAll(".sec-5-caixa-duvidas > div .btn-duvidas")
-
-const cxDuvidas = document.querySelector('.paragrafo-duvida')
-
-
-for(let btn of btnDuvidas){
-
-
-  btn.onclick = () => cxDuvidas.classList.toggle('active')
-
+   
 }
-
