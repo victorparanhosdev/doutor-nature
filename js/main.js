b6f64952928ca0303c2f454cd728362df3f1aaaf
Victor Paranhos <victor_paranhos@hotmail.com>
2023-02-02 12:20:52 -0300

    02 de Fev

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ new Caixinhas()
 
 
 
+
+
+const btnDuvidas = document.querySelectorAll(".sec-5-caixa-duvidas > div .btn-duvidas")
+
+const cxDuvidas = document.querySelector('.sec-5-caixa-duvidas')
+
+
+for(let btn of btnDuvidas){
+
+
+  btn.onclick = () => cxDuvidas.classList.toggle('active')
+
+}
+
